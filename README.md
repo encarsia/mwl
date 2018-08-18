eac119a035a3fb877a6ab1d94c51aaead4fa0115
encarsia <An.Ke@bahnfreikartoffelbrei.de>
2018-08-18 17:37:40 +0200

    initial

---
+++ README.md
@@ -0,0 +1,153 @@
+# mwl - Mauerweglaufdaten analysieren
+
+## Wozu?
+
+* Planungshilfe für die [100 Meilen Berlin](http://100meilen.de/), Neugier, Zahlenspielerei
+
+## Was kann angezeigt werden?
+
+* Streckeninfo, Ranking, Durchlaufzeiten an den VPs, Läuferdetails
+
+## Daten
+
+* die Daten wurden von [SPORTident](https://www.sportident.com/) erfasst und sind als csv-Dateien verfügbar (oben rechts unter "Downloads" auf der jeweiligen Veranstaltungsseite)
+* die von der SPORTident-Webseite heruntergeladenen csv-Dateien sind nicht UTF-8-kodiert
+
+## Benutzung
+
+* Repository klonen oder Archiv herunterladen und entpacken und ins entsprechende Verzeichnis wechseln
+* Python-Interpreter starten
+
+### Daten laden
+
+```python
+from analyze100miles import Results
+res = Results(2014) # lädt die Daten des Jahres 2014
+```
+
+### Streckeninfo
+
+```python
+print(res.course_info)
+```
+
+#### Ausgabe
+
+```
+Verpflegungspunkte/Zeitmessung
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+VP    Split km  Abs km    Name
+---------------------------------------------------------------------------------
+VP1   7,3       7,3       Brandenburger Tor
+VP2   6,46      13,76     East Side Gallery
+[...]
+VP27  3,02      157,94    Wollankstrasse
+Ziel  3,96      161,9     Friedrich-Ludwig-Jahn-Sportpark
+---------------------------------------------------------------------------------
+```
+
+### Durchlaufzeiten an den VPs
+
+```python
+res.vp_stats("VP5")
+
+# Filtern nach Frauen/Männern/Staffeln
+res.vp_stats("VP5", "f") 
+res.vp_stats("VP5", "m") 
+res.vp_stats("VP5", "r")
+```
+
+#### Ausgabe
+
+```
+VP5 - U-Bahnhof Rudow - km 31,2
+************************************************************************
+Anzahl Läufer: 254
+1.:  8:20:08 Uhr (Nagata, Tsutomu)
+2.:  8:24:18 Uhr (Perkins, Mark)
+3.:  8:25:07 Uhr (Bonfiglio, Marco)
+4.:  8:26:24 Uhr (Morstabilini, luca)
+5.:  8:27:00 Uhr (Tribius, Thomas)
+6.:  8:38:49 Uhr (Brade, Benjamin)
+7.:  8:38:55 Uhr (Klug, Matthias)
+8.:  8:39:00 Uhr (Strykowski, Jaroslaw)
+9.:  8:41:20 Uhr (Lingg, Dieter)
+10.: 8:42:32 Uhr (Kuehner, Christof)
+
+25 %:   9:13:54 Uhr
+50 %:   9:35:07 Uhr
+75 %:   9:53:32 Uhr
+100% :  11:07:10 Uhr (Läufer, Besen)
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Split pace in min/km
+
+1.:  4:54 (Bonfiglio, Marco)
+2.:  5:03 (Perkins, Mark)
+3.:  5:03 (Morstabilini, luca)
+4.:  5:05 (Zugspitz Trailer)
+5.:  5:09 (Nagata, Tsutomu)
+************************************************************************
+```
+
+### Alle Durchlaufzeiten als Textdatei ausgeben
+
+```python
+res.stats_to_file()
+
+# Filtern nach Frauen/Männern/Staffeln
+res.stats_to_file("f") 
+res.stats_to_file("m") 
+res.stats_to_file("r")
+```
+
+### Ranking
+
+```python
+print(res.ranking)
+```
+
+### Ausgabe
+
+```
+Ranking
+~~~~~~~
+Platz StartNr Name                                     Zeit       Kategorie
+---------------------------------------------------------------------------------
+1     177     Perkins, Mark                            13:06:52   Senioren M30 (30-34 Jahre)
+2     199     Bonfiglio, Marco                         14:04:27   Senioren M35 (35-39 Jahre)
+[...]
+1     266     Seidel, Grit                             18:16:29   Seniorinnen W40 (40-44 Jahre)
+[...]
+15    10000   Läufer, Besen                            29:01:19   Staffel 4 x 40 km
+[...]
+```
+
+### Läuferdetails
+
+```python
+res.runner_stats(100) # StartNr, siehe Ranking
+```
+
+#### Ausgabe
+
+```
+Name: Deák, Andreas - Platz: 169
+StartNr: 100 - Kategorie: Senioren M50 (50-54 Jahre)
+Zeit: 28:23:58 - Pace: 10:35 min/km
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+VP    Split time Split pace Time (total)
+VP1   01:04:55   7:11       1:04:55
+VP2   00:41:11   6:53       1:46:06
+[...]
+VP26  01:02:08   10:52      27:10:19
+VP27  00:30:12   9:46       27:40:31
+Ziel  00:43:27   11:33      28:23:58
+```
+
+## Übertragbarkeit auf andere Veranstaltungen
+
+* **ungetestet**
+* da die csv-Dateien immer den gleichen Aufbau haben, sollte sich das Skript auch leicht an andere Veranstaltungen anpassen lassen:
+  * ``vp_list.yaml``: Details der Messstationen anpassen/erstellen
+  * ``analyze100miles.py``: Dictionary ``YEAR_COURSE`` entsprechend anpassen
+  * csv-Datei(en) in ``/SI_results`` speichern
---
+++ analyze100miles.py
@@ -0,0 +1,304 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import collections
+import csv
+import datetime
+import yaml
+
+Runner = collections.namedtuple("Runner", [
+            "rank",
+            "startnr",
+            "name",
+            "cat",
+            "tag",
+            "starttime",
+            "time",
+            "pace",
+            "stages",
+            ])
+
+Stage = collections.namedtuple("Stage", [
+            "time",
+            "pace",
+            "time_total",
+            ])
+
+YEAR_COURSE = { 2011 : "clockwise_2011",
+                2013 : "counterclockwise_2013",
+                2014 : "clockwise",
+                2015 : "counterclockwise",
+                2016 : "clockwise",
+                2017 : "counterclockwise",
+                2018 : "clockwise_new",
+                }
+
+VP_FILE = "vp_list.yaml"
+
+RULE = "---------------------------------------------------------------------------------\n"
+
+
+class Results:
+    
+    def __init__(self, year):
+        if year in YEAR_COURSE.keys():
+            self.year = year
+            
+            self.vp_list = self._read_vplist(VP_FILE, self.year)
+            self.course_info = self._get_course(self.vp_list)
+            self.vp_index = list(self.vp_list.keys())
+            
+            csv_file = "SI_results/result_course_{}.csv".format(self.year)
+            data = self._read_csv(csv_file)
+            self.results = self._runner_details(data, self.vp_index)
+            self.ranking = self._get_ranking(self.results)
+
+        else:
+            print("Available options (int): {}".format(YEAR_COURSE.keys()))
+
+    def _read_vplist(self, filename, year):
+        """read yaml file and returns data as dict"""
+        with open(filename) as f:
+            vp_list = yaml.load(f)
+        vp_list = vp_list[YEAR_COURSE[year]]
+        return vp_list
+
+    def _read_csv(self, filename):
+        """returns results from SportIdent csv file as list"""
+        with open(filename) as f:
+            reader = csv.reader(f, delimiter=";")
+            data = list(reader)
+        return data
+
+    def _get_course(self, vp_list):
+        returnstring = ("""
+Verpflegungspunkte/Zeitmessung
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+""")
+        row = "{:<5} {:<9} {:<9} {}\n"
+        returnstring += row.format("VP", "Split km", "Abs km", "Name")
+        returnstring += RULE
+        for vp in vp_list:
+            returnstring += row.format(vp,
+                              vp_list[vp]["km"],
+                              vp_list[vp]["km_kum"],
+                              vp_list[vp]["name"],
+                              )
+        returnstring += RULE
+        return returnstring
+
+    def _get_ranking(self, results):
+        returnstring = ("""
+Ranking
+~~~~~~~
+""")
+        row = "{:<5} {:<7} {:<40} {:<10} {}\n"
+        returnstring += row.format("Platz", "StartNr", "Name", "Zeit", "Kategorie")
+        returnstring += RULE
+        for r in results:
+            returnstring += row.format(r.rank,
+                                       r.startnr,
+                                       r.name,
+                                       r.time,
+                                       r.cat,
+                                    )
+        returnstring += RULE
+        return returnstring
+
+    def _sort_pace(self, pace):
+        try:
+            hour, minute = pace[0].split(":")
+            return int(hour), int(minute)
+        except ValueError:
+            return
+
+    def _runner_details(self, data, vp_index):
+        """reads data list and returns list of namedtuples with list of
+           runner details and stage times"""
+        res = []
+        for d in data:
+            if d[1] != "StartNr": # ignore table headers
+                stage_time = []
+                stage_pace = []
+                stage_total = []
+                total = datetime.timedelta()
+                for t in d[15:97:3]:
+                    stage_time.append(t)
+                    try:
+                        h, m, s = t.split(":")
+                        delta = datetime.timedelta(hours=int(h), minutes=int(m), seconds=int(s))
+                        total += delta
+                    except ValueError:
+                        total = t
+                        break
+                    stage_total.append(total)
+                for p in d[16:98:3]:
+                    stage_pace.append(p.split(" min/km")[0])
+                
+                stages = dict()
+                for i, t, p, tot in zip(vp_index, stage_time, stage_pace, stage_total):
+                    stages[i] = Stage(t, p, tot)
+
+                if d[8].startswith("Senioren") or d[8].startswith("Männer"):
+                    tag = "m"
+                    starttime = datetime.timedelta(hours=6)
+                elif d[8].startswith("Seniorinnen") or d[8].startswith("Frauen"):
+                    tag = "f"
+                    starttime = datetime.timedelta(hours=6)
+                else:
+                    tag = "r"
+                    starttime = datetime.timedelta(hours=7)
+                    
+                res.append(Runner(d[0],
+                                  int(d[1]),
+                                  d[3],
+                                  d[8],
+                                  tag,
+                                  starttime,
+                                  d[11],
+                                  d[12],
+                                  stages,
+                                 ))
+        return res
+
+    def vp_stats(self, vp, tag="all"):
+        print(self._get_vp_stats(vp, tag))
+
+    def stats_to_file(self, tag="all"):
+        """creates list for all vps and dumps it into file"""
+        data = "Durchlaufzeiten für das Jahr {}\n".format(self.year)
+        for vp in self.vp_index:
+            data += self._get_vp_stats(vp, tag)
+        filename = "{}_{}.txt".format(self.year, tag)
+        with open(filename, "w") as f:
+            f.write(data)
+
+    def _get_vp_stats(self, vp, tag):
+        """reads all runner results at a given vp and returns string with
+           first 3/quartils times of passing the vp.
+           
+           Arguments:
+                vp: pass vp index as string, p.e. "VP1"..."Ziel"
+                cat: "all" (default): include single runners and relays
+                     "f": women's results only
+                     "m": men's results only
+                     "r": relay results only
+        """
+        pass_all = []
+        pace = []
+        for r in self.results:
+            try:
+                pass_time = r.starttime + r.stages[vp].time_total
+                # probably measurement error at Sportident
+                # 2018: startno 2001, total time of > 42 hours...?
+                if pass_time > datetime.timedelta(days=2):
+                    raise KeyError
+                # ignore if stage pace is missing
+                if r.stages[vp].pace == "-":
+                    raise KeyError
+                if tag == r.tag:
+                    try:
+                        pass_all.append([pass_time, r.name])
+                        pace.append((r.stages[vp].pace, r.name))
+                    except KeyError:
+                        pass
+                elif tag == "all":
+                    try:
+                        pass_all.append([pass_time, r.name])
+                        pace.append((r.stages[vp].pace, r.name))
+                    except KeyError:
+                        pass
+            except KeyError:
+                pass
+        pass_all = sorted(pass_all)
+        # just for readability convenience
+        for p in pass_all:
+            p[0] = str(p[0]).replace("1 day", "Sonntag")
+        pace = sorted(pace, key=self._sort_pace)
+        returnstring = """
+{} - {} - km {}
+************************************************************************
+Anzahl Läufer: {}
+1.:  {} Uhr ({})
+2.:  {} Uhr ({})
+3.:  {} Uhr ({})
+4.:  {} Uhr ({})
+5.:  {} Uhr ({})
+6.:  {} Uhr ({})
+7.:  {} Uhr ({})
+8.:  {} Uhr ({})
+9.:  {} Uhr ({})
+10.: {} Uhr ({})
+
+25 %:   {} Uhr
+50 %:   {} Uhr
+75 %:   {} Uhr
+100% :  {} Uhr ({})
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Split pace in min/km
+
+1.:  {} ({})
+2.:  {} ({})
+3.:  {} ({})
+4.:  {} ({})
+5.:  {} ({})
+************************************************************************
+""".format(vp,
+               self.vp_list[vp]["name"],
+               self.vp_list[vp]["km_kum"],
+               len(pass_all),
+               pass_all[0][0], pass_all[0][1],
+               pass_all[1][0], pass_all[1][1],
+               pass_all[2][0], pass_all[2][1],
+               pass_all[3][0], pass_all[3][1],
+               pass_all[4][0], pass_all[4][1],
+               pass_all[5][0], pass_all[5][1],
+               pass_all[6][0], pass_all[6][1],
+               pass_all[7][0], pass_all[7][1],
+               pass_all[8][0], pass_all[8][1],
+               pass_all[9][0], pass_all[9][1],
+
+               pass_all[int(len(pass_all) * .25)][0],
+               pass_all[int(len(pass_all) * .5)][0],
+               pass_all[int(len(pass_all) * .75)][0],
+               pass_all[-1][0], pass_all[-1][1],
+               
+               pace[0][0], pace[0][1],
+               pace[1][0], pace[1][1],
+               pace[2][0], pace[2][1],
+               pace[3][0], pace[3][1],
+               pace[4][0], pace[4][1],
+              )
+        return returnstring
+
+    def runner_stats(self, nr):
+        for r in self.results:
+            if r.startnr == nr:
+                print("""
+Name: {} - Platz: {}
+StartNr: {} - Kategorie: {}
+Zeit: {} - Pace: {}
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~""".format(r.name,
+                                                              r.rank,
+                                                              nr,
+                                                              r.cat,
+                                                              r.time,
+                                                              r.pace
+                                                              ))
+                row = "{:<5} {:<10} {:<10} {}"
+                print(row.format("VP",
+                                  "Split time",
+                                  "Split pace",
+                                  "Time (total)",
+                                  ))
+                for stage in r.stages:
+                    t = r.stages[stage].time_total.total_seconds()
+                    time_total = "{}:{:>02}:{:>02}".format(int(t // 3600),
+                                                   int(t % 3600 // 60),
+                                                   int(t % 60,))
+                    print(row.format(stage,
+                                      r.stages[stage].time,
+                                      r.stages[stage].pace,
+                                      time_total,
+                                      ))
+                break
